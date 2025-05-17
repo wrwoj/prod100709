87a52a64eb7d09d355e7a587cde28950e30ebee6
wrwoj <w.r.wojtyna@gmail.com>
2025-05-17 15:37:07 +0200

    1st commit

---
+++ src/main/java/com/example/prod100709/Prod100709Application.java
@@ -0,0 +1,13 @@
+package com.example.prod100709;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prod100709Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prod100709Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prod100709/Prod100709ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prod100709;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prod100709ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
